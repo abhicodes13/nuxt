2c7827266c82f05b19f964136e0affe3d7f5ba7c
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-22 15:30:42 -0400

    first

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<!-- pages/about.vue -->
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>This is the about page of my Nuxt app.</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+const a = "Hello";
+</script>
+
+<template>
+  <h1>{{ a }}</h1>
+</template>
+
+<style>
+body {
+  background: black;
+}
+h1 {
+  color: skyblue;
+}
+</style>
