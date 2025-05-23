9abdb6b76203d9b71448f274df9dbdfa0adcb9f9
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-23 12:25:55 -0400

    config file update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  app: {
+    head: {
+      title: "Nuxt API fetch",
+    },
+  },
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/tailwindcss"],
+});
