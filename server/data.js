6abffde1d3de79643d8b373113d8289a86a66ca3
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-31 23:41:23 -0400

    mock db data

--- server/data.js
+++ server/data.js
@@ -2,7 +2,7 @@
 export const posts = [
   { id: 1, title: "Hello World", content: "This is my first post" },
   { id: 2, title: "Second Post", content: "Another great read" },
-  { id: 3, title: "Third Post", content: "Another great read" },
-  { id: 4, title: "Fourth Post", content: "Another great read" },
-  { id: 5, title: "Fifth Post", content: "Another great read" },
+  { id: 3, title: "Third Post", content: "Third Post" },
+  { id: 4, title: "Fourth Post", content: "Fourth Post " },
+  { id: 5, title: "Fifth Post", content: "Fifth Post" },
 ];
